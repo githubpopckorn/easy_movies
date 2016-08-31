bf256f82e01f15684197a6c2da183970acde6ffa
Santy <santy_lopez@live.com.ar>
2016-08-31 01:00:27 -0500

    Agregado concerns para compartir el código de subidas de un archivo a un post y un attachment

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,33 +1,4 @@
 class Attachment < ApplicationRecord
   belongs_to :post
-  after_save :guardar_archivo
-    
-  PATH_ARCHIVOS = File.join Rails.root, "public", "archivos"
-  def archivo=(archivo)
-      unless archivo.blank?
-          @archivo = archivo
-          self.nombre = archivo.original_filename
-          self.extension = archivo.original_filename.split(".").last.downcase
-      end
-  end
- 
-  def path_archivo
-      File.join PATH_ARCHIVOS, "#{self.id}.#{self.extension}"
-  end
-    
-  def tiene_archivo?
-      File.exists? path_archivo
-  end
-    
-  private
-  def guardar_archivo
-      if @archivo
-          FileUtils.mkdir_p PATH_ARCHIVOS
-          File.open(path_archivo,"wb") do |f|
-              f.write(@archivo.read)
-          end
-          @archivo = nil
-      end
-  end
-    
+  include PicturableConcern
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :attachments
   validates :title, presence: true, uniqueness: true
-  
+  include PicturableConcern
 end
